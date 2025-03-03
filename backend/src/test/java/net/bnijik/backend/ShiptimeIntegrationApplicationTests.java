c969d469dea6c369a68b7f689ac899de8550e5e1
Boris Nijikovsky <borisnijikovsky@gmail.com>
2025-03-03 16:09:40 -0500

    Initial commit

---
+++ backend/src/test/java/net/bnijik/backend/ShiptimeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bnijik.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiptimeIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
