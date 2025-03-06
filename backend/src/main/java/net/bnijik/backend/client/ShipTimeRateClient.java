14946a5632d9444f1d55dbc99cdc979ff77feb46
Boris Nijikovsky <borisnijikovsky@gmail.com>
2025-03-06 14:35:50 -0500

    Cleanup

--- backend/src/main/java/net/bnijik/backend/client/ShipTimeRateClient.java
+++ backend/src/main/java/net/bnijik/backend/client/ShipTimeRateClient.java
@@ -1,7 +1,7 @@
 package net.bnijik.backend.client;
 
-import net.bnijik.backend.payload.ShipTimeRateResponse;
 import net.bnijik.backend.payload.externalApi.ShipTimeRateRequest;
+import net.bnijik.backend.payload.externalApi.ShipTimeRateResponse;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
