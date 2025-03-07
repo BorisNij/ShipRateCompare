be048ac1baf907b85ed7c34778464b6cf4393565
Boris Nijikovsky <borisnijikovsky@gmail.com>
2025-03-07 00:51:42 -0500

    Implement rate controller

---
+++ backend/src/main/java/net/bnijik/backend/controller/rest/RateController.java
@@ -0,0 +1,25 @@
+package net.bnijik.backend.controller.rest;
+
+import lombok.RequiredArgsConstructor;
+import net.bnijik.backend.payload.RateRequest;
+import net.bnijik.backend.payload.RateResponse;
+import net.bnijik.backend.service.RateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/api/rates")
+public class RateController {
+    @Autowired
+    private final RateService rateService;
+
+    @PostMapping
+    public ResponseEntity<RateResponse> getRates(@RequestBody RateRequest rateRequest) {
+        return ResponseEntity.ok(rateService.getRates(rateRequest));
+    }
+}
