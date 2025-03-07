2fe5548889636676edd0f1a8b4d30231e9d290c7
Boris Nijikovsky <borisnijikovsky@gmail.com>
2025-03-06 20:56:08 -0500

    Set expected Content-Type for ShipTimeRateClient requests

--- backend/src/main/java/net/bnijik/backend/client/ShipTimeRateClient.java
+++ backend/src/main/java/net/bnijik/backend/client/ShipTimeRateClient.java
@@ -2,11 +2,12 @@ package net.bnijik.backend.client;
 
 import net.bnijik.backend.payload.externalApi.ShipTimeRateRequest;
 import net.bnijik.backend.payload.externalApi.ShipTimeRateResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.service.annotation.HttpExchange;
 import org.springframework.web.service.annotation.PostExchange;
 
-@HttpExchange(url = "/rest")
+@HttpExchange(url = "/rest", contentType = MediaType.APPLICATION_JSON_VALUE)
 public interface ShipTimeRateClient {
     @PostExchange("/rates")
     ShipTimeRateResponse getRates(@RequestBody ShipTimeRateRequest shipTimeRateRequest);
