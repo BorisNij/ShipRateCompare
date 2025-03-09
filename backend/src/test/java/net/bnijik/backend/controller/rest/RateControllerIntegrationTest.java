e1a5595147374533d7fddc8b4bf23064e7640a7c
Boris Nijikovsky <borisnijikovsky@gmail.com>
2025-03-09 19:44:56 -0400

    Debug integration test

--- backend/src/test/java/net/bnijik/backend/controller/rest/RateControllerIntegrationTest.java
+++ backend/src/test/java/net/bnijik/backend/controller/rest/RateControllerIntegrationTest.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -16,12 +17,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@WebMvcTest(RateController.class)
-
-//@SpringJUnitConfig(classes = {ShipTimeClientConfig.class, ShipTimeClientLogger.class, RateServiceImpl.class, RateController.class, RateResponseConverterImpl.class}, initializers = ConfigDataApplicationContextInitializer.class)
-//@ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@EnabledIf(expression = "#{environment['spring.profiles.active'].matches('.*devlocal.*|.*sandbox.*')}", loadContext = true)
 public class RateControllerIntegrationTest {
 
     @Autowired
@@ -29,16 +27,11 @@ public class RateControllerIntegrationTest {
 
     private final ObjectMapper OBJECT_MAPPER = Fixtures.OBJECT_MAPPER;
 
-
-//    @Autowired
-//    private RateController rateController;
-
     @Test
     void getRates_ShouldReturnSuccessfulResponse() throws Exception {
         RateRequest rateRequest = Fixtures.createRateRequest();
 
-        String responseJson = mockMvc.perform(post("/api/rates")
-                                                      .contentType(MediaType.APPLICATION_JSON)
+        String responseJson = mockMvc.perform(post("/api/rates").contentType(MediaType.APPLICATION_JSON)
                                                       .content(OBJECT_MAPPER.writeValueAsString(rateRequest)))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
